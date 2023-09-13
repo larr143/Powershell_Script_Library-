0a4bd03faf48ea288eefc554269b17d76b930ae5
Larr143 <Larrytieken@gmail.com>
2023-09-13 13:15:18 -0500

    Testing

--- CSC512-playGround.py
+++ CSC512-playGround.py
@@ -1,6 +1,9 @@
+import psutil
+
+
 def main(): 
-    print('hi')
+    boolean = ("chrome.deb" in (i.name() for i in psutil.process_iter()))
+    print(boolean)
     
 if __name__ == "__main__":
     main()
-    
