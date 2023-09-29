e9fd563f349b4ed55e14c9462cada6cc6cbb62b2
Larr143 <Larrytieken@gmail.com>
2023-09-29 14:57:10 -0500

    fun with tk

--- CSC512-playGround.py
+++ CSC512-playGround.py
@@ -1,9 +1,22 @@
-import psutil
+import tkinter as tk
 
 
 def main(): 
-    boolean = ("chrome.deb" in (i.name() for i in psutil.process_iter()))
-    print(boolean)
+    root = tk.Tk()
+    root.title("Script Library")
+    root.minsize(300, 300)
+    root.geometry("300x300+50+50")
+
     
+    openingLabel = tk.Label(root,text = "Welcome to your Script Library")
+    openingLabel.place(y=(root.winfo_height()/3), x=(root.winfo_width()/3))    
+    
+    button = tk.Button(root,text ="click me")
+    button.place(y=(root.winfo_height()/2), x=(root.winfo_width()/2))
+    
+
+    root.mainloop()
+    
+
 if __name__ == "__main__":
     main()
