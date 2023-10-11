113b3c98e104456630117dec9c59e3ae9c5239bc
Larr143 <LarryTieken@gmail.com>
2023-10-11 10:17:30 -0500

    continuous testing of making the file structure

--- CSC512-ConfigTest.py
+++ CSC512-ConfigTest.py
@@ -1,5 +1,39 @@
+import os 
+
 def main():
-    print("hi")
+    #creating the library for the user or finding current library 
+    dirList = os.listdir()
+    
+    if 'scriptLibrary' in dirList:
+        dirList = os.listdir(os.path.join(os.getcwd() + '/scriptLibrary'))
+        
+        if 'ScriptLibrary.txt' in dirList:
+            configFile = open((os.path.join(os.getcwd() + '/scriptLibrary' + "/ScriptLibrary.txt")), "w+")
+        
+        else:
+            cwd = os.getcwd()
+            path = os.path.join(cwd, "scriptLibrary", 'ScriptLibrary.txt')
+            configFile = open(path, 'x')
+            
+    else:
+        cwd = os.getcwd()
+        path = os.path.join(cwd, "scriptLibrary")
+        os.makedirs(path)
+        dirList = os.listdir(os.path.join(os.getcwd() + '/scriptLibrary'))
+        
+        if 'ScriptLibrary.txt' in dirList:
+            configFile = open((os.path.join(os.getcwd() + '/scriptLibrary' + "/ScriptLibrary.txt")), "w+")
+
+        else:
+            cwd = os.getcwd()
+            path = os.path.join(cwd, "scriptLibrary", 'ScriptLibrary.txt')
+            configFile = open(path, 'x')
+        
+
+    configFile.write('hi')
+    configFile.close()
+        
+    
     
 if __name__ == "__main__":
     main()
